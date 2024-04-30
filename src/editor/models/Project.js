c680fb967bd822e8bae444cb47984c92f36e39c7
VoidEyeGaming <lunareyegaming@gmail.com>
2024-04-30 17:05:43 -0400

    Basic functionality for multi-directory node importing

--- src/editor/models/Project.js
+++ src/editor/models/Project.js
@@ -8,7 +8,11 @@ this.b3editor = this.b3editor || {};
 
   p.initialize = function() {
     this.fileName = null;
+
+    // There are two separate variables for all of the nodes paths to ensure compatibility with the unmodified version
+    // of this editor.
     this.nodesPath = null;
+    this.otherNodesPaths = null;
     this.trees = [];
   }
 
@@ -16,13 +20,23 @@ this.b3editor = this.b3editor || {};
     var config = JSON.parse(json);
     var project = new b3editor.Project()
     project.fileName = filename;
+
     project.nodesPath = path.resolve(filename, config.nodesPath);
+  
+    // If nodesPaths is defined, resolve each path. Otherwise, make project.nodesPaths an empty list.
+    if (config.otherNodesPaths) {
+      project.otherNodesPaths = config.otherNodesPaths.map(file => path.resolve(filename, file));
+    } else {
+      project.otherNodesPaths = [];
+    }
+  
     return project;
   }
 
   p.save = function() {
     var config = {}
     config.nodesPath = path.relative(this.fileName, this.nodesPath);
+    config.otherNodesPaths = this.otherNodesPaths.map(file => path.relative(this.fileName, file))
     return JSON.stringify(config, null, 2);
   }
 
@@ -40,7 +54,21 @@ this.b3editor = this.b3editor || {};
   }
 
   p.findNodes = function() {
-    return this.walk(this.nodesPath, /\.nodes$/);
+    var nodesPathNodes = this.walk(this.nodesPath, /\.nodes$/);
+
+    // If otherNodesPaths is null, simply return nodesPathNodes.
+    if (!this.otherNodesPaths) {
+      return nodesPathNodes;
+    }
+
+    var otherNodesPathsNodes = this.otherNodesPaths.map(file => this.walk(file, /\.nodes$/));
+    // Concat all otherNodesPathNodes into one list, then concatenate nodesPathNodes with it.
+    var allNodes = [];
+    otherNodesPathsNodes.forEach(fileList => {
+      allNodes = allNodes.concat(fileList);
+    })
+    allNodes = allNodes.concat(nodesPathNodes);
+    return allNodes;
   }
 
   p.findTrees = function() {
